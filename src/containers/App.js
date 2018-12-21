8772da11c5c47b45855eaa32584608aa386acfa2
Su <suce126@126.com>
2018-12-21 23:40:00 +0100

    fix a bug

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,26 +25,24 @@ class App extends Component {
     }
 
     render() {
-        const {robots, searchfield} = this.state;
+        const { robots, searchfield } = this.state;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (robots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
+        return !robots.length ?
+            <h1>Loading</h1> :
+            (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                    <CardList robots={filteredRobots} />
+                        <CardList robots={filteredRobots} />
                     </Scroll>
                 </div>
-            )
-
-        }
+            );
 
     }
+
 }
 
 export default App;
